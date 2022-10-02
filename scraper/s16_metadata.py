92fb4353b2e2e0d6adc458c1cabae5635ada3452
KJ.O <nomeansno@Live.ca>
2022-10-02 14:52:19 +0900

    Initial commit

---
+++ scraper/s16_metadata.py
@@ -0,0 +1,9 @@
+DRIVER_PATH = '/Users/audaefi/chromedriver/chromedriver'
+# '/usr/bin/chromedriver'
+
+IMG_SRC_CSS = ".img"
+HREF_CSS = ".mojar-element-image > a"
+PRODUCT_TITLE_CSS = ".title"
+PRICE_CSS = ".price"
+SELLER_CSS = ".company-name"
+NEXT_ARROW_BTN_CSS = ".btn-next"
